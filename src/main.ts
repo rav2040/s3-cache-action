a25244498319e97e26e543526843f8640379f9a6
Stuart Kennedy <rav2040@gmail.com>
2023-04-10 07:43:12 +1200

    Use key instead of run number

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,39 @@
 import { execFileSync } from "child_process";
 import { join } from "path";
 import { getInput, setFailed } from "@actions/core";
-import { context as gitHubContext } from "@actions/github";
+import globby from "globby";
+
+const PATH_SPLIT_REGEX = /\s+(?=([^"]*"[^"]*")*[^"]*$)/g;
 
 async function main() {
     try {
         const bucket = getInput("bucket");
         const path = getInput("path");
-        const runNumber = getInput("run-number") || null;
+        const key = getInput("key");
+        const download = getInput("download") === "true";
 
-        const paths = path.split("\n").map((str) => str.trim()).filter(Boolean);
+        const paths = (await Promise.all(
+            path.split(PATH_SPLIT_REGEX)
+                .filter(Boolean)
+                .map((path) => globby(path))
+        )).flat();
         const uniquePaths = Array.from(new Set(paths));
 
         uniquePaths.forEach((path) => {
             const localPath = join(process.cwd(), path);
-            const remotePath = `s3://${bucket}/${runNumber ?? gitHubContext.runNumber}/${path}`
+            const remotePath = `s3://${bucket}/${key}/${path}`;
 
-            const srcPath = runNumber === null ? localPath : remotePath;
-            const destPath = runNumber === null ? remotePath : localPath;
+            const srcPath = download ? remotePath : localPath;
+            const destPath = download ? localPath : remotePath;
 
-            if (runNumber === null) {
-                execFileSync("aws", ["s3", "rm", destPath, "--recursive", "--quiet"], { stdio: "inherit" });
-            }
-
-            execFileSync("aws", ["s3", "cp", srcPath, destPath, "--recursive", "--no-progress"], { stdio: "inherit" });
+            execFileSync("aws", [
+                "s3",
+                "cp",
+                srcPath,
+                destPath,
+                "--recursive",
+                "--no-progress"
+            ], { stdio: "inherit" });
         });
     } catch (err) {
         if (err instanceof Error) setFailed(err);
