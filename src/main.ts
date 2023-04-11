92b461120e6928a81e900c597fc47cb8327ba0e7
Stuart Kennedy <rav2040@gmail.com>
2023-04-12 08:34:19 +1200

    Test

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function main() {
         console.log("paths:", paths);
 
         const uniquePaths = Array.from(new Set(paths))
-            .filter((a, i, arr) => !arr.some((b, j) => i !== j && a.startsWith(b)));
+            .filter((a, i, arr) => !arr.some((b, j) => i !== j && b.at(-1) !== "/" && a.startsWith(b)));
 
         console.log("uniquePaths:", uniquePaths);
 
