775ba37fd4cb27c5b9563c3f2aabd3ce2cae318a
Stuart Kennedy <rav2040@gmail.com>
2023-04-12 08:47:47 +1200

    Use file read stream; Include empty dirs

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,35 @@
-import { join } from "path";
-import { readFile } from "fs/promises";
-import { getInput, setFailed } from "@actions/core";
+import { join, posix } from "path";
+import { createReadStream } from "fs";
+import { getInput, getMultilineInput, setFailed } from "@actions/core";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import globby from "globby";
 
-const PATH_SPLIT_REGEX = /\s+(?=([^"]*"[^"]*")*[^"]*$)/g;
-
 async function main() {
     try {
-        const bucket = getInput("bucket");
-        const path = getInput("path");
+        const bucket = getInput("bucket", { required: true });
+        const path = getMultilineInput("path", { required: true });
         const prefix = getInput("prefix");
 
-        const paths = (await Promise.all(
-            path.split(PATH_SPLIT_REGEX)
-                .filter(Boolean)
-                .map((path) => globby(path))
-        )).flat();
-        const uniquePaths = Array.from(new Set(paths));
+        const paths = (await Promise.all(path.map((path) => {
+            return globby(path, { onlyFiles: false, markDirectories: true });
+        }))).flat();
+
+        // Filter out directories that are common prefixes.
+        const uniquePaths = Array.from(new Set(paths))
+            .filter((a, _, arr) => !arr.some((b) => a !== b && b.startsWith(a)));
 
         const s3 = new S3Client({});
 
         await Promise.all(uniquePaths.map(async (path) => {
+            const isDir = path.at(-1) === "/";
+            const key = posix.join(prefix, path);
             const filePath = join(process.cwd(), path);
-            const file = await readFile(filePath);
+            const body = isDir ? undefined : createReadStream(filePath);
 
             const putObjectCommand = new PutObjectCommand({
                 Bucket: bucket,
-                Key: [prefix, path].filter(Boolean).join("/"),
-                Body: file
+                Key: key,
+                Body: body
             });
 
             await s3.send(putObjectCommand);
