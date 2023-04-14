2d3663137015702faa4bad76e75a1301cc0bb018
Stuart Kennedy <rav2040@gmail.com>
2023-04-14 14:27:45 +1200

    Test archive feature

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { join, posix } from "path";
 import { createReadStream } from "fs";
 import { stat } from "fs/promises";
-import { getInput, getMultilineInput, setFailed } from "@actions/core";
+import { getBooleanInput, getInput, getMultilineInput, setFailed } from "@actions/core";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { create as tarCreate } from "tar";
 import globby from "globby";
 
 const s3 = new S3Client({});
@@ -12,45 +13,67 @@ async function main() {
         const bucket = getInput("bucket", { required: true });
         const path = getMultilineInput("path", { required: true });
         const prefix = getInput("prefix");
+        const archive = getBooleanInput("archive");
 
-        const paths = (await Promise.all(path.map((path) => {
+        const aggregatedPaths = (await Promise.all(path.map((path) => {
             return globby(path, { onlyFiles: false, markDirectories: true });
         }))).flat();
 
         // Filter out directories that are common prefixes.
-        const uniquePaths = Array.from(new Set(paths))
+        const uniquePaths = Array.from(new Set(aggregatedPaths))
             .filter((a, i, arr) => {
                 return a.at(-1) !== "/" || !arr.some((b, j) => i !== j && b.startsWith(a) && b.length > a.length)
             });
 
-        let filesUploaded = 0;
-
-        await Promise.all(uniquePaths.map(async (path) => {
-            const isDir = path.at(-1) === "/";
-            const key = posix.join(prefix, path);
-            const filePath = join(process.cwd(), path);
-            const body = isDir ? undefined : createReadStream(filePath);
-            const contentLength = isDir ? undefined : (await stat(filePath)).size;
+        if (archive) {
+            const key = posix.join(prefix, "archive.tgz");
+            const tarStream = tarCreate({ gzip: true }, uniquePaths);
 
             const putObjectCommand = new PutObjectCommand({
                 Bucket: bucket,
                 Key: key,
-                Body: body,
-                ContentLength: contentLength,
+                Body: tarStream,
             });
 
             const response = await s3.send(putObjectCommand);
 
             if (response.$metadata.httpStatusCode === 200) {
-                filesUploaded++;
-                console.info("Uploaded:", filePath);
+                console.info("Uploaded archive:", "archive.tgz");
             }
-        }));
-
-        console.log("### Total files uploaded:", filesUploaded, "###");
+        } else {
+            await uploadFiles(bucket, prefix, uniquePaths);
+        }
     } catch (err) {
         if (err instanceof Error) setFailed(err);
     }
+}
+
+async function uploadFiles(bucket: string, prefix: string, paths: string[]) {
+    let filesUploaded = 0;
+
+    await Promise.all(paths.map(async (path) => {
+        const isDir = path.at(-1) === "/";
+        const key = posix.join(prefix, path);
+        const filePath = join(process.cwd(), path);
+        const body = isDir ? undefined : createReadStream(filePath);
+        const contentLength = isDir ? undefined : (await stat(filePath)).size;
+
+        const putObjectCommand = new PutObjectCommand({
+            Bucket: bucket,
+            Key: key,
+            Body: body,
+            ContentLength: contentLength,
+        });
+
+        const response = await s3.send(putObjectCommand);
+
+        if (response.$metadata.httpStatusCode === 200) {
+            filesUploaded++;
+            console.info("Uploaded:", filePath);
+        }
+    }));
+
+    console.log("### Total files uploaded:", filesUploaded, "###");
 }
 
 main();
